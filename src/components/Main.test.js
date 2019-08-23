3c827b0f2de5a0e4b27bccde31133540e6005f1f
Rachael Otto <rachaelotto@Rachaels-MacBook-Pro.local>
2019-08-22 17:00:59 -0700

    Test passing for onChange

--- src/components/Main.test.js
+++ src/components/Main.test.js
@@ -15,5 +15,14 @@ describe('Main', ()=>{
     expect(component.find({name: 'email'}).exists()).toBe(true);
   });
 
+  it('should update state when values are input', ()=> {
+    const component = shallow(<Main />);
+    let initState = component.state('email');
+    component.find({ name: 'email' }).simulate('change', { target: { name: "email", value: "test" } });
+    let changedState = component.state('email');
+    expect(initState).not.toBe(changedState);
+
+  })
+
 
 })

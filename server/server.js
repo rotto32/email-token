361477e25862f7e66bede44d34a24225f4edb962
Rachael Otto <rachaelotto@Rachaels-MacBook-Pro.local>
2019-08-22 18:24:34 -0700

    Fix bodyparser issue

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const { bodyParser } = require('body-parser');
+const bodyParser = require('body-parser');
 
 const { generateToken } = require('./token-generation.js');
 

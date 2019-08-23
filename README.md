9e195fa5715fdc96b22574b02c2c3e41a1872013
Rachael Otto <rachaelotto@Rachaels-MacBook-Pro.local>
2019-08-22 18:14:25 -0700

    Add sql schema, update README, add db connection

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ## Email Token Generator
 
+### Getting started
+
+Make sure MySQL @5.7 is installed and running on your computer, then run ```mysql -u root < db/schema.sql``` from the root directory of the repo.
---
+++ db/dbConnection.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql');
+const { connectionInfo } = require('../connectionInfo.js');
+
+const connection = mysql.createConnection(connectionInfo);
+
+
+const addTokenToDB = (req, cb) => {
+  connection.connect((connectionErr) => {
+    if (connectionErr) console.log(connectionErr);
+    connection.query(
+      '',
+      (queryErr, results) => {
+        if (queryErr) console.log(queryErr);
+        cb(results);
+      },
+    );
+  });
+};
+
+
+module.exports = { addTokenToDB };

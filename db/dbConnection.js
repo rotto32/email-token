6ed566d88885b3a6d149ce75a9e31f5294570e2a
Rachael Otto <rachaelotto@Rachaels-MacBook-Pro.local>
2019-08-22 18:16:30 -0700

    Add full function to insert data into the db

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -3,12 +3,11 @@ const { connectionInfo } = require('../connectionInfo.js');
 
 const connection = mysql.createConnection(connectionInfo);
 
-
 const addTokenToDB = (req, cb) => {
   connection.connect((connectionErr) => {
     if (connectionErr) console.log(connectionErr);
     connection.query(
-      '',
+      `INSERT INTO email_tokens (email, token) VALUES (${req.email}, ${req.token})`,
       (queryErr, results) => {
         if (queryErr) console.log(queryErr);
         cb(results);

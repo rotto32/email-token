f7229a122e6c7566dc28c3eb3d7f9798badb4f97
Rachael Otto <rachaelotto@Rachaels-MacBook-Pro.local>
2019-08-22 17:14:22 -0700

    Add all the front-end code that can be done without the backend

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import axios from "axios";
 
 class Main extends React.PureComponent {
   constructor() {
@@ -18,14 +19,32 @@ class Main extends React.PureComponent {
     })
   }
 
+  handleSubmit(e) {
+    e.preventDefault();
+    axios.post('localhost:80/token', this.state.email)
+      .catch((err)=>{
+        console.log(err);
+      })
+      .then((resp)={
+        //set state with token
+      })
+
+  }
+
   render () {
     return (
       <div>
         <h1>Generate Email Token</h1>
         <form>
           Enter email: <br />
-          <input type="email" name="email" className="email" onChange={this.handleChange}/>
+          <input type="email" name="email" onChange={this.handleChange}/>
+          <br />
+          <button onClick={this.handleSubmit}>Generate Token</button>
         </form>
+        Token:
+        <div>
+          {this.state.token}
+        </div>
       </div>
     );
   }
--- src/components/Main.test.js
+++ src/components/Main.test.js
@@ -18,10 +18,25 @@ describe('Main', ()=>{
   it('should update state when values are input', ()=> {
     const component = shallow(<Main />);
     let initState = component.state('email');
-    component.find({ name: 'email' }).simulate('change', { target: { name: "email", value: "test" } });
+    component
+      .find({ name: 'email' })
+      .simulate('change', { target: { name: "email", value: "test" } });
     let changedState = component.state('email');
     expect(initState).not.toBe(changedState);
+  });
 
+  it('should have a button', ()=>{
+    const component = shallow(<Main />);
+    expect(component.find('button').exists()).toBe(true);
+  });
+
+  it('should return a token when the button is clicked', ()=>{
+    const component = shallow(<Main />);
+    component
+      .find({ name: "email" })
+      .simulate("change", { target: { name: "email", value: "test" } });
+    component.find('button').simulate('click');
+    expect(component.state('token')).not.toBe('');
   })
 
 
